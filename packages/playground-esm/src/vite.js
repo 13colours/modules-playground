81fb7e28da6a1f9cf89527df103ee206d33277c5
13Colours <tasker.prabhat@gmail.com>
2022-06-29 23:08:47 +1000

    Add vite module test

---
+++ packages/playground-esm/src/vite.js
@@ -0,0 +1,9 @@
+import myModule from 'module-exercise-vite'
+
+const { add } = myModule
+
+const args = process.argv.slice(2)
+const a = parseInt(args[0])
+const b = parseInt(args[1])
+
+console.log(`Sum of ${a} and ${b} is ${add(a, b)}`)
